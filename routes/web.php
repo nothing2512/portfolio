d2bd8b042318d4c2f26c4a931af718717ee2c2bb
nothing2512 <blank345red@gmail.com>
2021-10-02 02:18:19 +0700

    (+) Finish deploy

--- routes/web.php
+++ routes/web.php
@@ -22,28 +22,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PortfolioController::class, "index"])->name("home");
 
-//Route::prefix("dashboard")->group(function () {
-//    Route::get("/", [DashboardController::class, "index"])->name("dashboard");
-//    Route::get("/skills", [SkillController::class, "index"])->name("skills");
-//    Route::get("/specializations", [SpecializationController::class, "index"])->name("specializations");
-//    Route::get("/projects", [ProjectController::class, "index"])->name("projects");
-//    Route::get("/profile", [UserController::class, "index"])->name("profile");
-//    Route::get("/logout", [DashboardController::class, "logout"])->name("logout");
-//});
-//
-//Route::prefix("services")->group(function() {
-//    Route::post("/skill/{id?}", [SkillController::class, "store"])->name("skill.store");
-//    Route::post("/skill/{id}/delete", [SkillController::class, "destroy"]);
-//
-//    Route::post("/specialization/{id?}", [SpecializationController::class, "store"])->name("specialization.store");
-//    Route::post("/specialization/{id}/delete", [SpecializationController::class, "destroy"]);
-//
-//    Route::post("/project/{id?}", [ProjectController::class, "store"])->name("project.store");
-//    Route::post("/project/{id}/delete", [ProjectController::class, "destroy"]);
-//
-//    Route::post("/profile/update", [UserController::class, "update"])->name("profile.update");
-//});
-//
+Route::prefix("dashboard")->group(function () {
+    Route::get("/", [DashboardController::class, "index"])->name("dashboard");
+    Route::get("/skills", [SkillController::class, "index"])->name("skills");
+    Route::get("/specializations", [SpecializationController::class, "index"])->name("specializations");
+    Route::get("/projects", [ProjectController::class, "index"])->name("projects");
+    Route::get("/profile", [UserController::class, "index"])->name("profile");
+    Route::get("/logout", [DashboardController::class, "logout"])->name("logout");
+});
+
+Route::prefix("services")->group(function() {
+    Route::post("/skill/{id?}", [SkillController::class, "store"])->name("skill.store");
+    Route::post("/skill/{id}/delete", [SkillController::class, "destroy"]);
+
+    Route::post("/specialization/{id?}", [SpecializationController::class, "store"])->name("specialization.store");
+    Route::post("/specialization/{id}/delete", [SpecializationController::class, "destroy"]);
+
+    Route::post("/project/{id?}", [ProjectController::class, "store"])->name("project.store");
+    Route::post("/project/{id}/delete", [ProjectController::class, "destroy"]);
+
+    Route::post("/profile/update", [UserController::class, "update"])->name("profile.update");
+});
+
 Route::prefix("attachment")->group(function () {
     Route::post("/upload", [AttachmentController::class, "upload"])->name("attachment.upload");
     Route::get("/{path?}", [AttachmentController::class, "show"])
